d60c6b841a039d14c445184b4fb084951664b369
avati123 <ashtonvati1@gmail.com>
2022-06-13 21:54:03 +1000

    Finished Weather API

--- Dashboard/resources/js/main.js
+++ Dashboard/resources/js/main.js
@@ -1,63 +1,42 @@
-// This is just a sample app. You can structure your Neutralinojs app code as you wish.
-// This example app is written with vanilla JavaScript and HTML.
-// Feel free to use any frontend framework you like :)
-// See more details: https://neutralino.js.org/docs/how-to/use-a-frontend-library
-
-function showInfo() {
-    document.getElementById('info').innerHTML = `
-        ${NL_APPID} is running on port ${NL_PORT}  inside ${NL_OS}
-        <br/><br/>
-        <span>server: v${NL_VERSION} . client: v${NL_CVERSION}</span>
-        `;
-}
-
-function openDocs() {
-    Neutralino.os.open("https://neutralino.js.org/docs");
-}
-
-function openTutorial() {
-    Neutralino.os.open("https://www.youtube.com/watch?v=txDlNNsgSh8&list=PLvTbqpiPhQRb2xNQlwMs0uVV0IN8N-pKj");
-}
-
-function setTray() {
-    if(NL_MODE != "window") {
-        console.log("INFO: Tray menu is only available in the window mode.");
-        return;
-    }
-    let tray = {
-        icon: "/resources/icons/trayIcon.png",
-        menuItems: [
-            {id: "VERSION", text: "Get version"},
-            {id: "SEP", text: "-"},
-            {id: "QUIT", text: "Quit"}
-        ]
-    };
-    Neutralino.os.setTray(tray);
-}
-
-function onTrayMenuItemClicked(event) {
-    switch(event.detail.id) {
-        case "VERSION":
-            Neutralino.os.showMessageBox("Version information",
-                `Neutralinojs server: v${NL_VERSION} | Neutralinojs client: v${NL_CVERSION}`);
-            break;
-        case "QUIT":
-            Neutralino.app.exit();
-            break;
-    }
-}
-
 function onWindowClose() {
     Neutralino.app.exit();
-}
-
+};
 Neutralino.init();
 
-Neutralino.events.on("trayMenuItemClicked", onTrayMenuItemClicked);
-Neutralino.events.on("windowClose", onWindowClose);
 
-if(NL_OS != "Darwin") { // TODO: Fix https://github.com/neutralinojs/neutralinojs/issues/615
-    setTray();
+const apikey = "fc76db83538a9e83ddb491c82ae0ab92";
+const lat = "-37.90943570810887";
+const long = "145.04147315069252";
+const htemp = document.getElementById("temp");
+
+function displayResults (response) {
+    console.log(response)
+    let temp = parseInt(response.main.temp);
+    let feels_like = parseInt(response.main.feels_like);
+    let icon = response.weather[0].icon;
+    let description = response.weather[0].description;
+    console.log(temp);
+    console.log(feels_like);
+    console.log(icon);
+    console.log(description);
+    document.getElementById("feels-like").innerHTML = `Feels like ${feels_like}°C`;
+    htemp.innerHTML = `It is ${temp}°C`;
+    document.getElementById("icon").src = `https://openweathermap.org/img/wn/${icon}.png`
+    document.getElementById("description").innerText = description;
+
+}
+getWeather = () => {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${apikey}&units=metric`)
+    .then(response => {
+        return response.json();
+
+    }).then(displayResults)
 }
 
-showInfo();
+
+
+
+getWeather();
+displayResults();
+
+
